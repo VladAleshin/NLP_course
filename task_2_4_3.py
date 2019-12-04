ae766f343a4214ad0ffe19b6a1661fb96c3c6a67
Vlad Aleshin <45937396+VladAleshin@users.noreply.github.com>
2019-12-04 16:05:09 +0300

    Update task_2_4_3.py

--- task_2_4_3.py
+++ task_2_4_3.py
@@ -1,12 +1,9 @@
 import re
 import sys
 
-# модифицируйте это регулярное выражение
 TOKENIZE_RE = re.compile(r'[а-яё]+|[+-]?\d*[,.]?\d+|\S{1}', re.I)
-
 def tokenize(txt):
     return TOKENIZE_RE.findall(txt)
-
 for line in sys.stdin:
     print(' '.join(tokenize(line.strip().lower())))
 
